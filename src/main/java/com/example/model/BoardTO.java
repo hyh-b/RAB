32cd165340615ae7831a0635eff472318ef47124
Ji Hyeon <qazzaq.00.wlgus@gmail.com>
2023-07-19 17:36:31 +0900

    jihyeon

--- src/main/java/com/example/model/BoardTO.java
+++ src/main/java/com/example/model/BoardTO.java
@@ -13,18 +13,29 @@ public class BoardTO {
     private String u_content;
     private String u_wdate;
     private String u_filename;
-    private int u_filesize;
+    private long u_filesize;
     private int u_hit;
     private int u_commentcount;
     
-    private String m_profilename;
-    private String m_name;
+    private String u_profilename;
+    private String u_writer;
+    
+	// modify_ok
+	private String oldFilename;
+	private String uploadPath;
+	
+	public BoardTO() {
+		// TODO Auto-generated constructor stub
+		this.oldFilename = "";
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "BoardTO [u_seq=" + u_seq + ", m_seq=" + m_seq + ", u_subject=" + u_subject + ", u_category="
 				+ u_category + ", u_content=" + u_content + ", u_wdate=" + u_wdate + ", u_filename=" + u_filename
 				+ ", u_filesize=" + u_filesize + ", u_hit=" + u_hit + ", u_commentcount=" + u_commentcount
-				+ ", m_profilename=" + m_profilename + ", m_name=" + m_name + "]";
+				+ ", m_profilename=" + u_profilename + ", u_writer=" + u_writer + "]";
 	}
     
 }
