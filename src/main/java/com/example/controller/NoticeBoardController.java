9851e3ad11363ecda97c829fc74e8d28f4827867
jongwon lim <jjong7632@gmail.com>
2023-07-26 16:11:20 +0900

    admin page side bar update success

--- src/main/java/com/example/controller/NoticeBoardController.java
+++ src/main/java/com/example/controller/NoticeBoardController.java
@@ -262,48 +262,48 @@ public class NoticeBoardController {
 
 
 	
-//	@RequestMapping("/notice_board_write_ok.do")
-//	public Map<String, Object> writeOKAjax(HttpServletRequest request, @RequestParam("uploads") MultipartFile[] uploads) {
-//		CustomUserDetails customUserDetails = customUserDetailsService.getCurrentUserDetails();
-//		String m_seq=customUserDetails.getM_seq();
-//		NoticeBoardTO bto = new NoticeBoardTO();
-//	    NoticeAlbumTO ato = new NoticeAlbumTO();
-//	    
-//	    String subject = request.getParameter("n_subject");
-//	    bto.setN_subject(subject);
-//	    String content = request.getParameter("n_content");
-//	    bto.setN_content(content);
-//	    bto.setM_seq(Integer.parseInt(m_seq));
-//
-//	    Map<String, Object> response = new HashMap<>();
-//	    int flagAB = dao.writeOK(bto);
-//
-//	    int flag = 0;
-//	    if (flagAB == 1) {
-//	        for (MultipartFile file : uploads) {
-//	            try {
-//	                // Upload file to S3
-//	                String fileUrl = s3FileUploadService.upload(file);
-//
-//	                // If the file was uploaded successfully, save its data to the database
-//	                if (fileUrl != null) {
-//	                    ato.setN_seq(bto.getN_seq());
-//	                    ato.setNf_filename(fileUrl);
-//	                    ato.setNf_filesize(file.getSize());
-//	                    flag = abdao.noticeAlbum_ok(ato);
-//	                } else {
-//	                    //System.out.println("File upload failed for n_seq: " + bto.getN_seq());
-//	                }
-//	            } catch (Exception e) {
-//	                e.printStackTrace();
-//	            }
-//	        }
-//	    }
-//
-//	    response.put("flagAB", flagAB);
-//	    response.put("flag", flag);
-//	    return response;
-//	}
+	@RequestMapping("/notice_board_write_ok.do")
+	public Map<String, Object> writeOKAjax(HttpServletRequest request, @RequestParam("uploads") MultipartFile[] uploads) {
+		CustomUserDetails customUserDetails = customUserDetailsService.getCurrentUserDetails();
+		String m_seq=customUserDetails.getM_seq();
+		NoticeBoardTO bto = new NoticeBoardTO();
+	    NoticeAlbumTO ato = new NoticeAlbumTO();
+	    
+	    String subject = request.getParameter("n_subject");
+	    bto.setN_subject(subject);
+	    String content = request.getParameter("n_content");
+	    bto.setN_content(content);
+	    bto.setM_seq(Integer.parseInt(m_seq));
+
+	    Map<String, Object> response = new HashMap<>();
+	    int flagAB = dao.writeOK(bto);
+
+	    int flag = 0;
+	    if (flagAB == 1) {
+	        for (MultipartFile file : uploads) {
+	            try {
+	                // Upload file to S3
+	                String fileUrl = s3FileUploadService.upload(file);
+
+	                // If the file was uploaded successfully, save its data to the database
+	                if (fileUrl != null) {
+	                    ato.setN_seq(bto.getN_seq());
+	                    ato.setNf_filename(fileUrl);
+	                    ato.setNf_filesize(file.getSize());
+	                    flag = abdao.noticeAlbum_ok(ato);
+	                } else {
+	                    //System.out.println("File upload failed for n_seq: " + bto.getN_seq());
+	                }
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    response.put("flagAB", flagAB);
+	    response.put("flag", flag);
+	    return response;
+	}
 	@RequestMapping("/notice_board_delete_ok.do")
 	public Map<String, Object> notice_board_delete(HttpServletRequest request, NoticeBoardTO noticeBoardTO)  {
 		Map<String, Object> response = new HashMap<>();
